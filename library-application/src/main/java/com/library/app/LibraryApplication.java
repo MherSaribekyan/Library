552a43e2fbeaef663f358f8bb33871f293e2c16b
mhers@crypmoji.com <mhers@crypmoji.com>
2023-08-02 17:57:27 +0400

    built modular architecture

--- library-application/src/main/java/com/library/app/LibraryApplication.java
+++ library-application/src/main/java/com/library/app/LibraryApplication.java
@@ -1,13 +1,13 @@
-package com.interview.library;
+package com.library.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(value = "classpath:com/library/app/library-application-context.xml")
 public class LibraryApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
     }
-
 }
